457659f88adecab8dfebbf9241bdc06a1e26e7bf
Al-ice <617944292@qq.com>
2017-11-29 14:18:01 +0800

    Update and rename txt.c to read_a_list.c

--- read_a_list.c
+++ read_a_list.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	FILE* file = fopen("10.691m8h_20171127_gps.txt", "r");
+	FILE* file = fopen("textname.txt", "r");
 	char line[256];   //save a line
 	char str[20];    //save the second list
 	double result;   //save the value of the second list(changed to double first)
