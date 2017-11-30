88127bc0e41b120611e85b23e0e35ae456ca1360
Al-ice <617944292@qq.com>
2017-11-30 23:00:04 +0800

    Update write_a_file.c

--- write_a_file.c
+++ write_a_file.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	FILE* file = fopen("10.691m8h_20171127_gps.txt", "r");
+	FILE* file = fopen("filename.txt", "r");
 	char line[256];   //save a line
 	char str[20];    //save the second list
 	//double y;   //the value of the second list(changed to double first)
