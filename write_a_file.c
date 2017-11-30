7a3a5f0847ae787f963f884726cc8b47ce05e845
Al-ice <617944292@qq.com>
2017-11-30 22:57:01 +0800

    Update write_a_file.c

--- write_a_file.c
+++ write_a_file.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	FILE* file = fopen("filename.txt", "r");
+	FILE* file = fopen("10.691m8h_20171127_gps.txt", "r");
 	char line[256];   //save a line
 	char str[20];    //save the second list
 	//double y;   //the value of the second list(changed to double first)
@@ -23,23 +23,22 @@ int main(void)
 		sscanf(line, "%*s %s", str);//sscanf analysis the data by lines, pass the first line(see the usage of sscanf)
 		double y;
 		y = atof(str);     //change to double
-		double a[NUM],b[NUM];
+		double a[NUM];
 		if (i < NUM) {
 			a[i] = y;
-			b[i] = a[i];
+			//write a line
 			printf("a[%d]=%f\n",i,a[i]);
-			//wite a file
 			FILE *fpWrite = fopen("data.txt", "a+");
 				fprintf(fpWrite, "%f\n", a[i]);
 			fclose(fpWrite);
 		}
 		else {
 			for (j=0;j<NUM-1;j++)
-				b[j] = b[j+1];
-			b[NUM-1] = y;
-			printf("%f\n", b[NUM-1]);
+				a[j] = a[j+1];    //add a number in the end and wipe the first number
+			a[NUM-1] = y;
+			printf("%f\n", a[NUM-1]);
 			FILE *fpWrite = fopen("data.txt", "a+");
-			fprintf(fpWrite, "%f\n", b[j]);
+			fprintf(fpWrite, "%f\n", a[j]);
 			fclose(fpWrite);		
 		}
 		i = i + 1;
